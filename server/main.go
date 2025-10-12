9bcc10dbd5eca937fd350b636391c3a639360f00
Nick Grove <nickgrove.se@gmail.com>
2025-10-12 03:15:07 -0400

    Query Param Rebuild

--- server/main.go
+++ server/main.go
@@ -54,6 +54,7 @@ func owDataHandler(w http.ResponseWriter, r *http.Request) {
 	queryParams := r.URL.Query()
 
 	input := queryParams.Get("input")
+	log.Println(input)
 	owmap := queryParams.Get("map")
 	region := queryParams.Get("region")
 	role := queryParams.Get("role")
